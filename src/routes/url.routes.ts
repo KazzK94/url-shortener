bcfa17c3cbe9e7e2ca85def4ad83426b848ddd6e
Kazz <i94773007@gmail.com>
2025-02-21 00:15:52 +0100

    enhance URL validation and update service methods for clarity

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getUrl, shortenUrl } from '../services/url.service'
+import { getUrlFromShortKey, shortenUrl } from '../services/url.service'
 
 const router = Router()
 
@@ -9,17 +9,22 @@ router.get('/', async (_req, res) => {
 
 router.post('/shorten', async (req, res) => {
 	const { url } = req.body
-	if (url === null || url === undefined) {
+	if (url === null || url === undefined || url === '') {
 		res.status(400).json({ error: 'URL is required' })
 	}
-	const shortUrl = await shortenUrl(url)
-	res.json(shortUrl)
+	try {
+		const shortUrl = await shortenUrl(url)
+		res.json(shortUrl)
+	} catch (error) {
+		console.error(error)
+		res.status(400).json({ error: 'Invalid URL' })
+	}
 })
 
 router.get('/:shortKey', async (req, res) => {
 	const { shortKey } = req.params
 	try {
-		const url = await getUrl(shortKey)
+		const url = await getUrlFromShortKey(shortKey)
 		if (url === null) res.status(404).json({ error: 'URL not found' })
 		res.redirect(url.targetUrl)
 	} catch (error) {
--- src/services/url.service.ts
+++ src/services/url.service.ts
@@ -3,14 +3,19 @@ import { UrlModel } from '../models/url.model'
 import { nanoid } from 'nanoid'
 import { UrlDataType } from '../types'
 
-export const shortenUrl = async (targetUrl: string): Promise<UrlDataType> => {
+/** Receives a full url, then returns an object containing both the fullUrl and the shortKey (and visits: 0) */
+export async function shortenUrl(targetUrl: string): Promise<UrlDataType> {
+	if (targetUrl === undefined || targetUrl === null || targetUrl === '' || !isValidUrl(targetUrl)) {
+		throw new Error('Invalid URL')
+	}
 	const shortKey = nanoid(6)
 	const url = new UrlModel({ targetUrl, shortKey })
 	await url.save()
 	return url
 }
 
-export const getUrl = async (shortKey: string): Promise<UrlDataType> => {
+/** Receives a shortKey and returns an object containing both the fullUrl, the shortKey and the visits */
+export async function getUrlFromShortKey(shortKey: string): Promise<UrlDataType> {
 	console.log({ shortKey })
 	const url = await UrlModel.findOne({ shortKey })
 	if (url === null) {
@@ -19,4 +24,15 @@ export const getUrl = async (shortKey: string): Promise<UrlDataType> => {
 	url.visits += 1
 	await url.save()
 	return url
+}
+
+/** Receives a string and returns true if its a valid url, or false otherwise */
+function isValidUrl(url: string): boolean {
+	try {
+		const validUrl = new URL(url)
+		return validUrl.protocol === 'http' || validUrl.protocol === 'https'
+	} catch (error) {
+		console.log(error)
+		return false
+	}
 }
